505d6c11d2810791c67d7d67d0813ab24b5b6fe7
nurminni <nurminni@arcada.fi>
2024-01-17 11:47:16 +0200

    done

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,27 @@
-export default function handler(req, res) {
+import { sql } from '@vercel/postgres';
+
+export default async function handler(req, res) {
+
+    if (req.method == "POST") {
+        const { name, species, age } = req.body;
+        await sql`
+            INSERT INTO pets (
+                name, 
+                species, 
+                age
+                ) VALUES ( 
+                    ${name}, ${species}, ${age}
+                    );`;
+
+        res.status(200).json({status: "created new pet"})
+    }
+
+    const { rows } = await sql`SELECT * FROM pets`;
+
     res.status(200).json({
         method: req.method,
         hello: "World!",
-        env: process.env.FOO
+        env: process.env.FOO,
+        pets: rows
     });
 }
---
+++ api/initdb.js
@@ -0,0 +1,13 @@
+import { sql } from '@vercel/postgres';
+
+export default async function handler(req, res) {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS pets (id serial, name varchar);`;
+        await sql`ALTER TABLE pets ADD COLUMN IF NOT EXISTS species varchar;`;
+        await sql`ALTER TABLE pets ADD COLUMN IF NOT EXISTS age numeric;`;
+
+        return res.status(200).json({ status: 'success' });
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+}
